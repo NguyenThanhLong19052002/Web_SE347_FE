9411287db3dcb064d8247c70f882e55470d32ccc
duynh109 <fanboizblackpink@gmail.com>
2023-12-19 17:10:12 +0700

    home update reponsive

--- src/Pages/Home1/Component/ShowListProduct.jsx
+++ src/Pages/Home1/Component/ShowListProduct.jsx
@@ -1,25 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useNavigate } from 'react-router-dom';
 
 const ShowListProduct = ({ products }) => {
   const navigate = useNavigate();
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const calculateItemsToShow = () => {
+    if (screenWidth >= 1024) {
+      return 4;
+    } else if (screenWidth >= 740) {
+      return 3;
+    } else {
+      return 2;
+    }
+  };
 
   const handleProductClick = (productId) => {
     navigate(`/productsdetail/${productId}`);
   };
 
   const showProducts = () => {
-    return products.slice(currentIndex, currentIndex + 4).map((product, index) => (
+    const itemsToShow = calculateItemsToShow();
+    return products.slice(currentIndex, currentIndex + itemsToShow).map((product, index) => (
       <div key={index} className="product-item">
-        <img className="product-image" src={product.image} alt={product.name} />
+        <img onClick={() => handleProductClick(product._id)} className="product-image" src={product.image} alt={product.name} />
         <a onClick={() => handleProductClick(product._id)} className="detail-button">
           Chi tiết
-          <ArrowForwardIcon sx={{ marginLeft: '8px' }} fontSize="small" />
+          <ArrowForwardIcon className="icon-arrow-next" fontSize="small" />
         </a>
         <p className="product-name">{product.name}</p>
         <p className="product-price">{`${product.price}đ`}</p>
+        <p className="product-quantity-sold">{`đã bán ${product.quantity_sold}`}</p>
       </div>
     ));
   };
