3c95fbc19c1204f28a72f595f1a7262e5a820da0
your name <you@exemple.com>
2024-01-13 22:03:32 +0700

    search warehouse

--- src/admin/pages/Warehouse/warehouse_admin.jsx
+++ src/admin/pages/Warehouse/warehouse_admin.jsx
@@ -10,11 +10,25 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import styles from "./warehouse.module.css";
+import { Form } from "react-bootstrap";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button } from "react-bootstrap";
 
 function WarehouseAdmin() {
   const [products, setProducts] = useState([]);
-
+  const [userQuery, setUserQuery] = useState("");
+  const handleChangeUserQuery = (e) => {
+    setUserQuery(e.target.value);
+  };
   useEffect(() => {
+    if (userQuery === "") {
+      loadProducts();
+    } else {
+      searchProduct();
+    }
+  }, [userQuery]);
+  
+  const loadProducts = () => {
     axios
       .get("https://dialuxury.onrender.com/product")
       .then((response) => {
@@ -23,13 +37,47 @@ function WarehouseAdmin() {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  };
+  
+  const searchProduct = () => {
+    axios
+      .get(`http://localhost:3001/product/search?query=${userQuery}`)
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  
 
   return (
     <Container fluid>
       <div className={"border-l-3 py-4"}>
         <h3 style={{ color: "#646161" }}>Tồn kho</h3>
-        <br></br>
+        <div className="d-flex align-items-center justify-content-between my-3">
+        <Form className={"d-flex text-center"}>
+            <Form.Control
+              type="search"
+              placeholder="Tìm kiếm sản phẩm..."
+              className={"me-2 " + styles.formcontrol}
+              aria-label="Search"
+              value={userQuery}
+              onChange={handleChangeUserQuery}
+            />
+
+            <Button
+              variant="primary"
+              disabled="True"
+              // className={styles.button}
+              // onClick={() => {
+              //   navigate(`/admin/productsPage?query=${userQuery}`);
+              // }}
+            >
+              <SearchIcon />
+            </Button>
+          </Form>
+        </div>
         <TableContainer component={Paper} className={styles.table}>
           <Table sx={{ minWidth: 1200 }} aria-label="simple table">
             <TableHead>
